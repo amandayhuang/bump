f8b87a8e1dbe8bb4fc9ff6caac5bfcce3e4a16f1
amanda <amandayhuang@gmail.com>
2020-09-20 10:56:00 -0400

    Updates

---
+++ precache-manifest.2fe99269c014284780c4cc90e7fe7155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e2f6b4a770d76e502aa7692961dd6b",
+    "url": "/bump/index.html"
+  },
+  {
+    "revision": "631c496f179b454a6e35",
+    "url": "/bump/static/css/main.9b94a2a9.chunk.css"
+  },
+  {
+    "revision": "f1cb43f42183a8ac099d",
+    "url": "/bump/static/js/2.db126794.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/bump/static/js/2.db126794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631c496f179b454a6e35",
+    "url": "/bump/static/js/main.41d1cfdd.chunk.js"
+  },
+  {
+    "revision": "15c505cdfb9dc76d1a37",
+    "url": "/bump/static/js/runtime-main.9ce7a42c.js"
+  }
+]);
